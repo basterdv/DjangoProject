5fd53fd4a02fcd753811ca97d7ff5dcb5c58b5ae
basterdv <baster1@list.ru>
2025-08-08 09:46:47 +1000

    работа с формами login and register

--- static/js/js_vk_auth.js
+++ static/js/js_vk_auth.js
@@ -42,27 +42,35 @@
 // Получаем глобальный объект VK ID
 const VKID = window.VKIDSDK;
 
-// Настройка SDC
-VKID.config.set({
-//     В метод передать данные
-    app: 54015625, // ID Приложения
-    redirectUrl: 'http://localhost/users/login',
-    responseMode: VKID.ConfigResponseMode.Callback,
-    source: VKID.ConfigSource.LOWCODE,
-    scope: '', // Заполните нужными доступами по необходимости
+// Настройка SDK
+VKID.Config.init({
+  app: 54015625, // Идентификатор приложения.
+  redirectUrl: 'http://localhost/users/login', // Адрес для перехода после авторизации.
+  // state: '<случайно сгенерированный state>', // Произвольная строка состояния приложения.
+  // codeVerifier: '<ваш сгенерированный code_verifier>', // Параметр в виде случайной строки. Обеспечивает защиту передаваемых данных.
+  scope: 'email phone', // Список прав доступа, которые нужны приложению.
 });
 
-
-// Создание экземпляра кнопки.
-const oneTap = new VKID.OneTap();
-
-// Получение контейнера из разметки.
-const container = document.getElementById('VkIdSdkOneTap');
-
-// Проверка наличия кнопки в разметке.
-if (container) {
-    // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
-    oneTap.render({container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS})
-        .on(VKID.WidgetEvents.ERROR, handleError); // handleError — какой-либо обработчик ошибки.
-}
+// VKID.config.set({
+// //     В метод передать данные
+//     app: 54015625, // ID Приложения
+//     redirectUrl: 'http://localhost/users/login',
+//     responseMode: VKID.ConfigResponseMode.Callback,
+//     source: VKID.ConfigSource.LOWCODE,
+//     scope: '', // Заполните нужными доступами по необходимости
+// });
+//
+//
+// // Создание экземпляра кнопки.
+// const oneTap = new VKID.OneTap();
+//
+// // Получение контейнера из разметки.
+// const container = document.getElementById('VkIdSdkOneTap');
+//
+// // Проверка наличия кнопки в разметке.
+// if (container) {
+//     // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
+//     oneTap.render({container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS})
+//         .on(VKID.WidgetEvents.ERROR, handleError); // handleError — какой-либо обработчик ошибки.
+// }
 
