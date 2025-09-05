0e0d397673903b7e709b0f888cca274de21aa02c
baster <baster1@list.ru>
2025-09-05 15:16:55 +1000

    работа с формами login and register

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.contrib import auth, messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
@@ -105,13 +106,15 @@ def profile(request):
     # return render(request, 'users/profile.html', context)
 
 
-class UserProfileView(TemplateView):
+class UserProfileView(LoginRequiredMixin,TemplateView):
     template_name = 'users/profile.html'
+    # login_url = 'users/login'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
         if user.is_authenticated:
+
             try:
                 profile = CustomUser.objects.get(pk=user.pk)
                 context = {
@@ -129,8 +132,7 @@ class UserProfileView(TemplateView):
             # Обработка случая, когда пользователь не авторизован
             logger.warning('Accessing user profile without authentication')
             context['profile'] = None
-
-
+            return redirect(reverse('main:index'))
 
     # def get_object(self):
     #     return CustomUser.objects.get(pk=self.request.user.pk)
@@ -211,112 +213,3 @@ class OAuthCallbackView(View):
                 logger.error(f'Ошибка авторизации')
 
         return redirect('/users/login')
-
-# @requires_csrf_token
-# def vk_auth_callback(request):
-#     try:
-#         code = request.GET.get('code')
-#         device_id = request.GET.get('device_id')
-#         _type = request.GET.get('type')
-#         expires_in = request.GET.get('expires_in')
-#         # code_verifier = request.GET.get('code_verifier')
-#         state = request.GET.get('state')
-#
-#         code_verifier = 'b5i9j_JQn3YeOlghVuS4WgdKcjoMIpJ7jFiOBO7QKSAxXa42HKu90hpMWQTL6F1KyoRrCD7Uw2H2u6mCQTJm-FMHro-NOYyhaPTsZqdkyFAvM85WGu0wa4g47SNOdyXW'
-#
-#         TOKEN_EXCHANGE_URL = 'https://id.vk.com/oauth2/auth'
-#         USER_PUBLIC_DATA_EXCHANGE_URL = 'https://id.vk.com/oauth2/public_info'
-#         USER_INFO_DATA_EXCHANGE_URL = 'https://id.vk.com/oauth2/user_info'
-#
-#         payload = {
-#             'grant_type': 'authorization_code',
-#             'client_id': 54015625,
-#             'device_id': device_id,
-#             'code': code,
-#             'redirect_uri': 'http://localhost/users/vk_auth_callback',
-#             'code_verifier': code_verifier,
-#             # 'state': state,
-#
-#         }
-#
-#         # Отправляем POST-запрос к VK API
-#         # print(payload)
-#         response = requests.post(TOKEN_EXCHANGE_URL, data=payload,
-#                                  headers={'Content-Type': 'application/x-www-form-urlencoded'})
-#         data = response.json()
-#
-#         print(data)
-#         if 'access_token' in data:
-#             # print(response.json())
-#             access_token = data['access_token']
-#             id_token = data['id_token']
-#             expires_in = data['expires_in']
-#             # print(access_token)
-#             # print(id_token)
-#             # print(expires_in)
-#
-#             # Отправляем POST-запрос для получения публичных данных пользователя
-#             payload = {
-#                 'client_id': 54015625,
-#                 'id_token': id_token,
-#             }
-#
-#             response = requests.post(USER_PUBLIC_DATA_EXCHANGE_URL, data=payload,
-#                                      headers={'Content-Type': 'application/x-www-form-urlencoded'})
-#             data_user_public_info = response.json()
-#             # print(data_user_public_info)
-#
-#             # Отправляем POST-запрос для получения личных данных пользователя
-#             payload = {
-#                 'client_id': 54015625,
-#                 'access_token': access_token,
-#             }
-#
-#             response = requests.post(
-#                 USER_INFO_DATA_EXCHANGE_URL,
-#                 data=payload,
-#                 headers={'Content-Type': 'application/x-www-form-urlencoded'}
-#             )
-#             data_user_info = response.json()
-#             # print(data_user_info)
-#             user_id = data_user_info['user']['user_id']
-#             email = data_user_info['user']['email']
-#             first_name = data_user_info['user']['first_name']
-#             last_name = data_user_info['user']['last_name']
-#             avatar = data_user_info['user']['avatar']
-#             birthday = data_user_info['user']['birthday']
-#
-#             username = email
-#             # Поиск или создание пользователя в базе данных
-#             try:
-#                 user = CustomUser.objects.get(email=email)
-#                 logger.info(f'Пользователь с почтой {email} найден')
-#                 # login(request, username) # Логинимся
-#             except:
-#                 logger.error(f'Пользователь с потной {email} не найден')
-#                 user = CustomUser.objects.create_user(
-#                     username=first_name,
-#                     email=email
-#                 )
-#             # Логинимся
-#             user.first_name = first_name
-#             user.last_name = last_name
-#             user.avatar = avatar
-#             user.birthday = datetime.strptime(birthday, '%d.%m.%Y').date().strftime('%Y-%m-%d')
-#
-#             user.username = first_name
-#             user.set_password(user_id)
-#             user.save()
-#
-#             login(request, user)
-#
-#         else:
-#             logger.error(f'error')
-#
-#         template_name = 'users/vk_auth_callback.html'
-#
-#         return render(request, template_name)
-#     except:
-#         template_name = 'users/login.html'
-#         logger.error(f'error')
-#         return render(request, template_name)
